c6d24ef8b5dd115266c0a4405de45899c4f3183d
5ud03r5 <kmarek973@gmail.com>
2023-03-17 21:49:26 +0100

    changed profile styling

--- components/IdeasList.vue
+++ components/IdeasList.vue
@@ -1,5 +1,5 @@
 <template>
-    <div class="bg-slate-200 w-full p-2 flex flex-col space-y-2 overflow-y-auto rounded-md">
+    <div class="w-full p-2 flex flex-col space-y-2 overflow-y-auto rounded-md">
         <IdeaItem class="w-full" />
         <IdeaItem class="w-full" />
         <IdeaItem class="w-full" />
