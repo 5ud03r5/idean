5efae6b56eb869f521436cc342a833bc2f9f3b5b
5ud03r5 <kmarek973@gmail.com>
2023-03-28 20:36:48 +0200

    changed sizing for dev and idea items

--- pages/app/developers.vue
+++ pages/app/developers.vue
@@ -1,6 +1,6 @@
 <template>
     <div class="flex flex-col my-10 lg:ml-72 max-lg:ml-16">
-        <div class="flex flex-col justify-center mb-10 ">
+        <div class="flex flex-col justify-center mb-5 ">
             <p class="lg:text-[40px] max-lg:text-[30px] max-sm:text-[25px] font-bold text-center mb-5">Find developers</p>
             <input
                 class="bg-gray-100 shadow-md py-1 mx-auto px-2 md:w-[400px] outline outline-1 outline-gray-200 transition-all rounded-lg focus:outline-none "
