fe7fdf12e12a527f64c7179fc5bdf61a9f97b620
5ud03r5 <kmarek973@gmail.com>
2023-03-28 19:31:36 +0200

    changed Profile item, added search to ideas

--- components/pofile/SkillTag.vue
+++ components/pofile/SkillTag.vue
@@ -1,5 +1,6 @@
 <template>
-    <div class="bg-gray-900 text-gray-100 w-max pl-2 pr-1 rounded-md justify-center m-[1px]">
+    <div
+        class="bg-gray-900 text-gray-100 w-max pl-2 pr-1 rounded-md justify-center m-[1px] max-sm:text-[12px] max-sm:rounded-sm">
         <slot></slot>
     </div>
 </template>
--- pages/app/ideas.vue
+++ pages/app/ideas.vue
@@ -6,14 +6,18 @@
 
         <p class="lg:text-[40px] max-lg:text-[30px] max-sm:text-[25px] font-bold text-center mb-5">Discover ideas</p>
         <div class="mx-auto">
-            <input
-                class="bg-gray-100 shadow-md py-1 px-2 md:w-[400px] outline outline-1 outline-gray-200 transition-all rounded-lg focus:outline-none "
-                placeholder="Search..." />
+            <form @submit.prevent="searchRepository">
+                <input v-model="search" autocomplete="off"
+                    class="bg-gray-100 shadow-md py-1 px-2 md:w-[400px] outline outline-1 outline-gray-200 transition-all rounded-lg focus:outline-none "
+                    placeholder="Search..." />
+            </form>
+
         </div>
 
         <div class="">
 
-            <div class="flex flex-wrap justify-center w-full p-2 overflow-x-hidden overflow-y-auto rounded-md ">
+            <div v-if="!pending"
+                class="flex flex-wrap justify-center w-full p-2 overflow-x-hidden overflow-y-auto rounded-md ">
                 <IdeaItem v-for="item, index in ideas" :key="item.id" class="md:w-[600px] max-md:w-full m-2 "
                     :github="item.github" @update:github="editIdea($event, item, index)" :idea_owner_id="item.idea_owner.id"
                     :idea_owner_name="item.idea_owner.name" :title="item.title" :description="item.description"
@@ -31,11 +35,15 @@ definePageMeta({
     layout: 'app',
     middleware: 'auth'
 })
+const search = ref('')
 const uri = useState('uri', () => '/api/ideas')
 const scrollPosition = useState('scrollPosition', () => 0)
+
 const { data: ideas, pending, refresh } = await useLazyAsyncData('ideas', async () => await $fetch(uri.value))
-const searchRepository = async (value) => {
-    uri.value = `/api/ideas?search=${value}`
+
+
+const searchRepository = async () => {
+    uri.value = `/api/ideas?search=${search.value.toLowerCase()}`
     refresh('ideas')
 }
 
